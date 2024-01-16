ddeb77f2543d557e38614c586d6afa8a3ade75ee
nashdev255 <nashdev255@gmail.com>
2024-01-16 18:45:40 +0900

    Add anotations

---
+++ WeaponRank.js
@@ -0,0 +1,18 @@
+/**:
+ * @target      MZ
+ * @plugindesc  WeaponRank
+ * @author      nashdev255
+ * @help        武器に付与することで、
+ * @url         https://github.com/nashdev255/game-maker-plugins/WeaponRank.js
+ */
+
+/**:
+ * @param   maxRank
+ * @text    最大ランク
+ * @desc    武器の最大ランクを変更することができます。
+ * @default 5
+ * @type number
+ */
+
+'use strict';
+
